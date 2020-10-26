5d6038bd2ae661e3f07083bb4078d3931513908e
Raveesh Mittal <raveesh.mittal@sabre.com>
2020-10-26 22:50:57 +0530

    Added email logic

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import express from 'express';
 import cron from 'node-cron';
+import * as nodeMailer from "nodemailer";
+// @ts-ignore
+import sendgridTransport from "nodemailer-sendgrid-transport";
 
 interface product {
     productName: string,
@@ -11,6 +14,12 @@ interface product {
 }
 
 const app = express();
+
+const transporter = nodeMailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: process.env.SENDGRID_KEY
+    }
+}));
 
 const getProductPromise = (product: product) => {
     return new Promise(async (resolve, reject) => {
@@ -42,12 +51,37 @@ app.get('/scrape', async (req, res) => {
 
 app.get('/test', (req, res) => res.send(`Testing environment MY_TEST variable ${process.env.MY_TEST}`));
 
+const getHTMLforFilteredResults = (filtered: any) => {
+    let html = '<table border="1"><tr>';
+    html = html.concat('<th>Alias</th><th>Product Name</th><th>Current Price</th><th>Cut off Price</th>');
+    html = html.concat('</tr>')
+    filtered.forEach((product: any) => {
+        html = html.concat('<tr>');
+        html = html.concat(`<td>${product.alias}</td><td>${product.productName}</td><td>${product.price}</td><td>${product.cutOffPrice}</td>`);
+        html = html.concat('</tr>');
+    });
+    return html;
+}
+
 app.listen(process.env.PORT || 4500, () => {
     console.log('Server running on port 4500');
-    // cron.schedule("* * * * *", async () => {
-    //     console.log("Running Cron Job");
-    //     const results = await getAllData();
-    //     const filtered = results.filter((product: any) => product.price <= product.cutOffPrice);
-    //     console.log('filtered results', filtered);
-    // });
+    cron.schedule("* * * * *", async () => {
+        console.log("Running Cron Job");
+        const results = await getAllData();
+        const filtered = results.filter((product: any) => product.price <= product.cutOffPrice);
+        console.log('filtered results', filtered);
+        const htmlResults = getHTMLforFilteredResults(filtered);
+        console.log('html resilts', htmlResults);
+        try {
+            await transporter.sendMail({
+                from: `NodeUser@gmail.com`, // sender address
+                to: 'raveeshm2@gmail.com', // list of receivers
+                subject: "Filtered list", // Subject line
+                html: `Filtered results from heroku are: ${htmlResults}`
+            });
+        } catch (err) {
+            console.log('Error sending email');
+            console.log('err', err);
+        }
+    });
 });
