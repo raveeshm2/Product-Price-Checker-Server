4e6525c2c846f0605d82c8a3a4f4546a6eb5a023
Raveesh Mittal <raveesh.mittal@sabre.com>
2020-10-31 01:09:31 +0530

    Corrected changed password route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -27,9 +27,17 @@ router.post('/login', async (req, res, next) => {
 
 router.put('/change', async (req, res, next) => {
     const user = await userModel.findOne({});
-    user!.password = await bcrypt.hash(process.env.DEFAULT_PASSWORD!, 12);
-    await user!.save();
-    return res.status(200).send({ message: ["Password changed successfully"] });
+    const currentPassword = req.body.currentPassword;
+    const newPassword = req.body.newPassword;
+    if (!currentPassword || !newPassword)
+        return next(new Error('Please provide old and new passwords'));
+    const match = await bcrypt.compare(currentPassword, user!.password);
+    if (match) {
+        user!.password = await bcrypt.hash(newPassword, 12);
+        await user!.save();
+        return res.status(200).send({ message: ["Password changed successfully"] });
+    }
+    next(new Error("Incorrect old password provided"));
 });
 
 export default router;
