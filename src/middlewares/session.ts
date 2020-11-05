6281528d483dbcfe575dab72615226a02c6476cb
Raveesh Mittal <raveesh.mittal@sabre.com>
2020-11-05 22:33:32 +0530

    Removed HTTPS check for cookie

--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -11,7 +11,7 @@ export default session({
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: false,
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     },
     store: new MongoStore({
